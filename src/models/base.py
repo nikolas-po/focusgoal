8f86d3ce55ffecbd54f1d61c76469ff342cfb3e8
nikolas-po <tadoter36@gmail.com>
2026-05-21 13:40:24 +0300

    часовой пояс поменял на МСК

--- src/models/base.py
+++ src/models/base.py
@@ -1,12 +1,11 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, DateTime
-from datetime import datetime, timezone
+from datetime import datetime
 
 Base = declarative_base()
 
 class BaseModel(Base):
     __abstract__ = True
     id = Column(Integer, primary_key=True, autoincrement=True)
-    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
-    updated_at = Column(DateTime, default=lambda: datetime.now(timezone.utc),
-                        onupdate=lambda: datetime.now(timezone.utc), nullable=False)
+    created_at = Column(DateTime, default=datetime.now, nullable=False)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now, nullable=False)
